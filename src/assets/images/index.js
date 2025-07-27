91302087cf56849b991798168f2eef2556d09251
Sameer Trivedi <sameertrivedi1234@gmail.com>
2025-07-27 14:34:08 +0530

    optimzed media

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,29 +1,25 @@
-import coverImage from "./newPFP.png";
-import js from "./js.png";
-import node from "./NODE.png";
-import react from "./react.png";
-import mongo from "./mongo.png";
-import bootstrap from "./bs.png";
-import cpp from "./c++.png";
-import python from "./python.png";
-import azure from "./azure.png";
-import csharp from "./csharp.png"
-import gmail from "./gmail.png";
-import sql from "./sql.png"
-import rust from "./rust.png";
-import docker from "./docker.png";
-import aspnetcore from "./aspnetcore.png";
-import azurefunctions from "./azurefunctions.png";
-import angular from "./angular.png";
-import kubernetes from "./kubernetes.png";
-import tailwind from "./tailwind.png";
-import github from "./github.png";
-import linkedin from "./linkedin.png";
-import discord from "./discord_chat.png";
-import project1 from "./project_1.png";
-import project2 from "./project_2.png";
-import project3 from "./project_3.png";
-import project4 from "./project_4.png";
+import coverImage from "./newPFP.webp";
+import js from "./js.webp";
+import node from "./NODE.webp";
+import react from "./react.webp";
+import mongo from "./mongo.webp";
+import bootstrap from "./bs.webp";
+import cpp from "./c++.webp";
+import python from "./python.webp";
+import azure from "./azure.webp";
+import csharp from "./csharp.webp"
+import gmail from "./gmail.webp";
+import sql from "./sql.webp"
+import rust from "./rust.webp";
+import docker from "./docker.webp";
+import aspnetcore from "./aspnetcore.webp";
+import azurefunctions from "./azurefunctions.webp";
+import angular from "./angular.webp";
+import kubernetes from "./kubernetes.webp";
+import tailwind from "./tailwind.webp";
+import github from "./github.webp";
+import linkedin from "./linkedin.webp";
+import discord from "./discord_chat.webp";
 import macbook from "./macbook.webp";
 import blog from "./blog.webp";
 import coinkeeper from "./coinkeeper.webp";
@@ -53,10 +49,6 @@ export const images = {
   github,
   linkedin,
   discord,
-  project1,
-  project2,
-  project3,
-  project4,
   macbook,
   blog,
   coinkeeper,
